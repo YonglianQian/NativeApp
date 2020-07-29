5397d0e279cfbab870331818a4da69c6c817252a
Abraham Qian <qianyonglian@outlook.com>
2020-07-29 10:04:57 +0800

    Add something

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCenter.start(getApplication(), "9366e590f97ab4124d6f84bea023d7fb238157ee", Analytics.class, Crashes.class);
+
+        //Crashes.generateTestCrash();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 

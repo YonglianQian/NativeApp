f65910b453257ac725ea579fa80b796c0ad377ec
Abraham Qian <qianyonglian@outlook.com>
2020-08-14 16:36:16 +0800

    add a jks file and set up signingconfigs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,9 +15,7 @@ import com.microsoft.appcenter.utils.async.AppCenterConsumer;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +24,10 @@ public class MainActivity extends AppCompatActivity {
         AppCenter.start(getApplication(), "e7b84169-0750-412e-834f-eeccf1ce9b54", Analytics.class, Crashes.class, Distribute.class);
         AppCenter.setLogLevel(Log.VERBOSE);
         //Crashes.generateTestCrash();
-         TextView tv=new TextView(this);
-        tv.setText(stringFromJNI());
-
+        TextView tv=new TextView(this);
+        tv.setText("Hello, World");
         setContentView(tv);
-
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
 }
